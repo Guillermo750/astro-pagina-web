a1b3e561a79f9533ed426d0d762bf83a251c3bb6
Guillermo <gdp066351@gmail.com@gmail.com>
2025-04-21 12:13:34 +0100

    Cambios añadidos

---
+++ src/data/accesorios-data.js
@@ -0,0 +1,19 @@
+/* Estás definiendo y exportando una constante en JavaScript llamada accesoriosPorConsola. 
+Esta constante es un objeto que organiza los accesorios de diferentes consolas de videojuegos */
+export const accesoriosPorConsola = {
+  'Nintendo Switch': [
+    { nombre: 'Joy-Con Adicional', descripcion: 'Un par de mandos versátiles.', image: '../assets/joy-con-1024x576.webp'},
+    { nombre: 'Pro Controller', descripcion: 'Mando tradicional.', image: '../assets/controller.webp'},
+    { nombre: 'Funda de Protección', descripcion: 'Mantén tu consola portátil o mando a salvo de golpes.', image: '../assets/funda-de-proteccion.webp'}
+  ],
+  'PlayStation': [
+    { nombre: 'DualSense', descripcion: 'Mando innovador de PS5.', image: '../assets/dualsense-1024x683.webp'},
+    { nombre: 'PlayStation VR', descripcion: 'Sumérgete en mundos virtuales inmersivos con el dispositivo de realidad virtual de PlayStation.', image: '../assets/playStation-vr2-1-1024x576.webp'},
+    { nombre: 'PlayStation Plus', descripcion: 'Desbloquea todo el potencial de tu PlayStation con una suscripción a PlayStation Plus.', image: '../assets/playstation-plus-1.webp'}
+  ],
+  'Xbox': [
+    { nombre: 'Wireless Controller', descripcion: 'Mando cómodo y ergonómico.', image: '../assets/controlador-xbox.webp'},
+    { nombre: 'Xbox Game Pass Ultimate', descripcion: 'Obtén acceso ilimitado a una enorme biblioteca de cientos de juegos de alta calidad para consola.', image: '../assets/xbox_game_pass_ultimate-1024x576.webp'},
+    { nombre: 'Xbox Series X/S', descripcion: 'Experimenta la velocidad y el rendimiento de la última generación de consolas Xbox.', image: '../assets/Xbox-Series-S-1024x576.webp'}
+  ]
+};
