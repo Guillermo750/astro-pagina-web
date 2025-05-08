6a429303735bf38f73ed80c329bd15b0687c67e5
Guillermo <gdp066351@gmail.com@gmail.com>
2025-05-08 13:52:16 +0100

    Haciendo correcciones del código

--- db/config.ts
+++ db/config.ts
@@ -1,9 +1,10 @@
+// MIRAR LA DOCUMENTACIÓN --> https://docs.astro.build/en/guides/integrations-guide/db/
 // db/config.ts
 import { defineDb, defineTable, column, NOW } from 'astro:db';
 
 const temas = defineTable({
   columns: {
-    id: column.number({ primaryKey: true, autoIncrement: true }),
+    id_tema: column.number({ primaryKey: true, autoIncrement: true }),
     nombre: column.text({ length: 100, unique: true, notNull: true }),
     descripcion: column.text(),
     fecha_creacion: column.date({ default: NOW }),
@@ -14,9 +15,9 @@ const usuarios = defineTable({
   columns: {
     id: column.number({ primaryKey: true, autoIncrement: true }),
     id_user: column.number({ unique: true, notNull: true }),
-    nombre_usuario: column.text({ length: 50, notNull: true }),
+    nombre_usuario: column.text({ length: 50, unique: true, notNull: true }),
     email: column.text({ length: 100, unique: true, notNull: true }),
-    password: column.text({ length: 255, notNull: true }),
+    password: column.text({ length: 255, unique: true, notNull: true }),
     fecha_registro: column.date({default: NOW}),
   },
 });
@@ -24,43 +25,53 @@ const usuarios = defineTable({
 const publicaciones = defineTable({
   columns: {
     id_publica: column.number({ primaryKey: true, autoIncrement: true }),
-    id_user: column.number({ notNull: true }),
-    titulo: column.text({ length: 50, notNull: true }),
-    texto: column.text({ length: 50, notNull: true }),
-    id_tema: column.number({ notNull: true }),
+    id_user: column.number({ unique: true, notNull: true }),
+    titulo: column.text({ unique: true, length: 50, notNull: true }),
+    texto: column.text({ length: 50, unique: true, notNull: true }),
+    id_tema: column.number({ unique: true, notNull: true }),
     fecha_creacion: column.date({ default: NOW }),
     fecha_edicion: column.date({ default: NOW, onUpdate: NOW }),
   },
-
+  foreignKeys: [
+    {
+      columns: ["id_user", "id_tema"],
+      references: () => [usuarios.columns.id_user, temas.columns.id_tema],
+    },
+  ], 
 });
 
 const comentarios = defineTable({
   columns: {
-    id: column.number({ primaryKey: true, autoIncrement: true }),
-    id_usuario: column.number({ notNull: true }),
-    id_publicacion: column.number({ notNull: true }),
+    id_coment: column.number({ primaryKey: true, autoIncrement: true }),
+    id_usuario: column.number({ unique: true, notNull: true }),
+    id_publicacion: column.number({ unique: true, notNull: true }),
     contenido: column.text(),
     fecha_creacion: column.date({ default: NOW }),
     fecha_edicion: column.date(),
   },
-  // EJEMPLO DE CLAVE FORÁNEAS PARA LAS RELACIONES ENTRE LAS TABLAS
-  /* foreignKeys: [
+   foreignKeys: [
     {
-      columns: ["authorFirstName", "authorLastName"],
-      references: () => [Author.columns.firstName, Author.columns.lastName],
+      columns: ["id_usuario", "id_publicacion"],
+      references: () => [usuarios.columns.id_user, publicaciones.columns.id_publica],
     },
-  ], */
+  ], 
 });
 
 const votos = defineTable({
   columns: {
     id: column.number({ primaryKey: true, autoIncrement: true }),
-    id_usuario: column.number({ notNull: true }),
+    id_usuario: column.number({ unique: true, notNull: true }),
     id_publicacion: column.number(),
     id_comentario: column.number(),
     tipo_voto: column.enum(['up', 'down']).notNull(),
     fecha_voto: column.date({default: NOW}),
   },
+  foreignKeys: [
+    {
+      columns: ["id_usuario", "id_publicacion", "id_comentario"],
+      references: () => [usuarios.columns.id_user, publicaciones.columns.id_publica, comentarios.id_coment],
+    },
+  ], 
   indexes: [
     {
       on: ['id_usuario', 'id_publicacion', 'id_comentario'],
